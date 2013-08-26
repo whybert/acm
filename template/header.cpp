ac8508fc401cf708df92de3c67565fcb7117a263
whybert <whybert@163.com>
2013-08-26 22:01:29 +0800

    add header.cpp

---
+++ template/header.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <numeric>
+#include <string>
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define re(i,j,n) for(int i=j;i<n;i++)
+#define debug1(x) cout << #x" = " << x << endl;
+#define debug2(x, y) cout << #x" = " << x << " " << #y" = " << y << endl;
+#define debug3(x, y, z) cout << #x" = " << x << " " << #y" = " << y << " " << #z" = " << z << endl;
+#define debug4(x, y, z, w) cout << #x" = " << x << " " << #y" = " << y << " " << #z" = " << z << " " << #w" = " << w << endl;
+using namespace std;
+
+int main() {
+
+    return 0;
+}
